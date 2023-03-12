410d3dc0d209f23d40ca4ef79e3b6eac20b5620d
unko-chan <kevin@kliang.dev>
2023-03-12 08:53:33 -0700

    Update Commonpaths.js

--- src/Components/Commonpaths.js
+++ src/Components/Commonpaths.js
@@ -3,19 +3,24 @@ import Card from "./Card";
 import Trailblazer from "./Trailblazer";
 import careerpath from "./Careerpath";
 import { output } from "../Data/output";
-
+import { userData } from "../Data/userData.js";
+import { logJobHistory } from "../Scripts/JobLogging";
 
 export default function Commonpaths(props) {
-  const careerPaths = output[props.data.title].careerPaths;
+  const careerPaths = logJobHistory(userData)[props.data.title].careerPaths;  
   console.log(careerPaths);
   return (
     <>
       <div className="header">Common Paths</div>
 
-      <Card type="commonpaths" careerData={careerPaths} navTo="NextSteps" onUpdateComponent={props.onUpdateComponent} />
+      <Card
+        type="commonpaths"
+        careerData={careerPaths}
+        navTo="NextSteps"
+        onUpdateComponent={props.onUpdateComponent}
+      />
       {/* <Card />
       <Trailblazer /> */}
-
     </>
   );
 }
