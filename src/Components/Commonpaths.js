798e87e69da73329cc9c204ee16acab5a784be19
unko-chan <kevin@kliang.dev>
2023-03-12 06:36:14 -0700

    Implement common paths rendering using data input

--- src/Components/Commonpaths.js
+++ src/Components/Commonpaths.js
@@ -2,12 +2,19 @@ import React from "react";
 import Card from "./Card";
 import Trailblazer from "./Trailblazer";
 import careerpath from "./Careerpath";
+import { output } from "../Data/output";
 
-export default function Commonpaths() {
+
+export default function Commonpaths(props) {
+  const careerPaths = output[props.data.title].careerPaths;
+  console.log(careerPaths);
   return (
     <>
-      <Card />
-      <Trailblazer />
+      <div className="header">Common Paths</div>
+
+      <Card type="commonpaths" careerData={careerPaths} navTo="NextSteps" onUpdateComponent={props.onUpdateComponent} />
+      {/* <Card />
+      <Trailblazer /> */}
 
     </>
   );
